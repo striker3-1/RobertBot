a95760df7fe45b0a5756ea9ede3448839bfa7914
Lung Yeung LUI <23183830A@common.cpce-polyu.edu.hk>
2025-05-04 01:17:44 +0800

    init bot

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent 
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content.includes('hi')) {
+    message.channel.send('fuck you！');
+  }
+});
+
+client.login('process.env.TOKEN');
